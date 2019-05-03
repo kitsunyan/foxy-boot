f76b6c6a6ddadb515c3cc4d6e40de2d6f319d07f
kitsunyan <kitsunyan@airmail.cc>
2019-05-03 04:14:00 +0300

    Handle EBUSY magiskpolicy error

--- magisk/common/service.sh
+++ magisk/common/service.sh
@@ -1,2 +1,6 @@
 #!/system/bin/sh
-magiskpolicy --live 'allow bootanim kernel system syslog_read'
+while true; do
+  magiskpolicy --live 'allow bootanim kernel system syslog_read' 2>&1 |
+  grep -Fq 'Device or resource busy' || break
+  sleep 1
+done
